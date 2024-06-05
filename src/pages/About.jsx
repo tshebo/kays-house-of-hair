401937346e2cc99499fbaf1f6524ede5b1ed3589
tshebo <tshebosereetsi@gmail.com>
2024-06-05 14:02:21 +0200

    "Refactored About and Services pages: removed cards from Services page, rearranged and updated content on About page."

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,16 +20,7 @@ function About() {
                 About
                 <span style={{ color: "#ce4c7a" }}>Us</span>
               </h2>
-              <p className="p-2">
-                Khensani's House of Hair is a premium hair salon located in
-                Hammanskraal. We specialize in creating beautiful frontals and
-                providing high-quality hair products to our customers. Our
-                mission is to enhance your natural beauty and boost your
-                confidence with our exceptional services and products.
-              </p>
-              <h4 className="d-flex justify-content-center mb-2">Follow <span style={{ color: "#ce4c7a" }}>Us</span></h4>
-              <div className="social-links mb-3">
-               
+              <div className="social-links p-3 ">
                 <a
                   href="https://api.whatsapp.com/send?phone=%2B27767831191&context=ARApjTuFOG_BrE8E48GnE0pXAUe0Qgjll2sXMXsIlmMJajv9ujDE8vQZEswBKYzgNrEdzD5Eo5b0eqj9RWBW6hoR1YXcraAXtuAmquuGWizoiBmqkh4sGj-v2PUJJJP2yUPR6lkSxcyiDqRbkU_Jh2_j1Q&source=FB_Page&app=facebook&entry_point=page_cta"
                   target="_blank"
@@ -59,6 +50,13 @@ function About() {
                   <RiInstagramFill />
                 </a>
               </div>
+              <p className="p-2">
+                Khensani's House of Hair is a premium hair salon located in
+                Hammanskraal. We specialize in creating beautiful weaves and
+                providing high-quality hair products to our customers. Our
+                mission is to enhance your natural beauty and boost your
+                confidence with our exceptional services and products.
+              </p>
             </div>
           </Col>
           <Col md={6}>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -36,30 +36,7 @@ function Services() {
               footer="Last updated 3 mins ago"
             />
           </Col>
-          <Col lg={4} md={6} sm={12}>
-            <Card
-              image={img}
-              title="Card title 4"
-              text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content th"
-              footer="Last updated 3 mins ago"
-            />
-          </Col>
-          <Col lg={4} md={6} sm={12}>
-            <Card
-              image={img}
-              title="Card title 5"
-              text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content"
-              footer="Last updated 3 mins ago"
-            />
-          </Col>
-          <Col lg={4} md={6} sm={12}>
-            <Card
-              image={img}
-              title="Card title 6"
-              text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-              footer="K"
-            />
-          </Col>
+           
         </Row>
       </Container>
     </div>
